18b1bb9033ed3240942304d25a47dece64fc0278
bahasaracoglu <bahasaracoglu@gmail.com>
2023-08-07 15:57:46 +0300

    ArrayList Challange tamamlandı.

---
+++ src/com/workintech/model/arrayListChallange/GroceryList.java
@@ -0,0 +1,46 @@
+package com.workintech.model.arrayListChallange;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class GroceryList extends ArrayList {
+
+    private ArrayList<String> cart = new ArrayList<>();
+    public void addItems(String... items) {
+        for (String item : items) {
+            if (!checkItemIsInList(item)) {
+                cart.add(item);
+            } else {
+                System.out.println("Can not add " + " which is already in list.");
+            }
+        }
+        Collections.sort(cart);
+    }
+
+    public void removeItems(String... items) {
+        for (String item : items) {
+            if (checkItemIsInList(item)) {
+                cart.remove(item);
+            } else {
+                System.out.println("Can not remove " + item + " which is not in the list.");
+            }
+        }
+        Collections.sort(cart);
+    }
+
+    private boolean checkItemIsInList(String item) {
+        return cart.contains(item);
+    }
+
+    private void printSorted() {
+        Collections.sort(cart);
+        System.out.println(cart);
+    }
+
+    @Override
+    public String toString() {
+        return "GroceryList{" +
+                "cart=" + cart +
+                '}';
+    }
+}
---
+++ src/com/workintech/model/arrayListChallange/ShoppinCart.java
@@ -0,0 +1,33 @@
+package com.workintech.model.arrayListChallange;
+
+import java.util.Scanner;
+
+public class ShoppinCart {
+    public static void main(String[] args) {
+        GroceryList groceryList = new GroceryList();
+        Scanner sc = new Scanner(System.in);
+
+        boolean isOn = true;
+        while(isOn = true){
+            System.out.println("İlerlemek için seçim yapın:\n"+"0 Çıkış\n"+"1 Ekleme\n"+"2 Çıkarma");
+            int input = sc.nextInt();
+        switch (input) {
+            case (0) :
+                isOn = false;
+                break;
+            case (1):
+                System.out.println("Eklenmesini istediğiniz elemanları giriniz.");
+                String itemsToAdd = sc.next();
+                groceryList.addItems(itemsToAdd);
+                System.out.println(groceryList.toString());
+                break;
+            case (2):
+                System.out.println("Çıkarmak istediğiniz elemanları giriniz.");
+                String itemsToRemove = sc.next();
+                groceryList.removeItems(itemsToRemove);
+                System.out.println(groceryList.toString());
+                break;
+
+        }
+    }}
+}
